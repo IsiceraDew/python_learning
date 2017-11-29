cd3ae3f56f3a4f9946a6f164a5f5fcbbaf8ac180
IsiceraDew <mugenxero@gmail.com>
2017-11-28 21:13:49 -0800

    Sort by fitness completed. Cull the weak added. Create offspring added.

--- genetic_algo.py
+++ genetic_algo.py
@@ -1,11 +1,12 @@
 """
 Genetic Algorithm
 """
-
+import random
 import rng
+
 target_phrase = "The night is young and full of rest."
 length = len(target_phrase)
-pop_size = 50
+pop_size = 5000
 
 def run_genetic_algo(num_steps):
     creatures = init_creatures()
@@ -39,21 +40,34 @@ def fit_calc(creature):
 def sort_by_fitness(creatures):
     """Returns the input list of creatures sorted by fitness, i.e.,
     creatures[0] is the fittest."""
-    return creatures  # TODO: placeholder
+    fitness = []
+    for c in creatures:
+        fitness.append((fit_calc(c), c))
+    fitness.sort()
+    organized = []
+    for f in fitness:
+        organized.append(f[1])
+    return organized    
 
 def remove_worst(creatures):
     """Removes some of the worst creatures.
     Must always return a non-empty list.
-
+    
     Input is a sorted list.
     """
-    return creatures  # TODO: placeholder (homework possibility)
+    return creatures[:pop_size]
 
 def create_offspring(creatures):
     """Create some offspring through mutation and / or crossover."""
-    return []  # TODO: placeholder
+    mutants = []
+    for c in creatures:
+        i = random.randint(0,length-1)
+        mutant = c[:i] + rng.rchr() + c[i+1:]
+        mutants.append(mutant)
+    return mutants
+        
 
-result = run_genetic_algo(8)
+result = run_genetic_algo(5000)
 
 print "Winner:"
 print result[0]
--- rng.py
+++ rng.py
@@ -3,19 +3,22 @@ Random Number Generator
 """
 import time
 import datetime
-
+import random
 
 
 seed = int(datetime.datetime.now().microsecond)
 
-def rand():
+def myrand():
 	global seed
 	seed *= 104333401
 	seed += 714987293
 	return seed % 256
+
+def rchr():
+	return chr(random.randint(0,255))
+
 def rstring(length):
 	l = list()
 	for i in xrange(length):
-		c = chr(rand())
-		l.append(c)
+		l.append(rchr())
 	return "".join(l)
